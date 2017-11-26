3d9b9e4f5289e43b7d3260d0e8b5985c2cc6f650
Quang Minh Trinh <trmquang0109@gmail.com>
2017-11-26 15:11:02 +0700

    update with unowned example

--- TestProject/PushedViewController.swift
+++ TestProject/PushedViewController.swift
@@ -14,8 +14,9 @@ class PushedViewController: UIViewController {
     var customLabel: UILabel!
     var testObject: TestObject!
     var initedString: String!
+    var updatedTimes: Int = 1
     private var customBlock : (()->())!
-    
+    private var customBlockTimer: Timer?
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +26,14 @@ class PushedViewController: UIViewController {
         addCustomLabel()
         
         /*
-         If you write like this, it will not deinit, never! Because the closure still hold strong reference to self
+         // Uncomment this and comment the customBlock below, if you write like this, it will not deinit, never! Because the closure still hold strong reference to self
          customBlock = {
             self.initedString = "Custom"
          }
         */
         
         // Change inheritedString to Custom
+        
         customBlock = { [weak self] in
             if let selfVC = self {
                 selfVC.initedString = "Custom"
@@ -40,6 +42,14 @@ class PushedViewController: UIViewController {
         // call the customBlock to execute
         customBlock()
         customLabel.text = initedString
+        /*
+         // Uncomment this and comment the startWeakTimer velow, if you write like this, the view controller will be deinit but you will see a crash! Because the timer block is still not deallocated
+         // meanwhile the self view controller has been dealloc
+         // the unowned self is used as a reference if we are sure that self controller have lifetime longer that the execution of the timer
+         startTimerWithUnownedSelf()
+        */
+        
+        startTimerWithWeakSelf()
     }
     deinit {
         // check if view controller has been "really" deinit
@@ -71,5 +81,32 @@ class PushedViewController: UIViewController {
         let constraints: [NSLayoutConstraint] = [NSLayoutConstraint.init(item: customLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0),
                                                  NSLayoutConstraint.init(item: customLabel, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)]
         view.addConstraints(constraints)
+    }
+    func makeCustomUnownedBlock() {
+        customBlock = { [unowned self] in
+            self.initedString = "Updated no \(self.updatedTimes)"
+            self.customLabel.text = self.initedString
+            self.updatedTimes += 1
+        }
+        customBlock()
+    }
+    
+    func startTimerWithUnownedSelf () {
+        customBlockTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: {[unowned self] (timer) in
+            // The advantage of unowned is because we can make sure the lifetime of block execution is shorter that self, we don't need to wrap self as optional. In addition, it is like weak ref, which
+            // means it allow the self view controller to dealloc right the time its object reference set to nil (pop view controller)
+            self.makeCustomUnownedBlock()
+            
+        })
+        customBlockTimer?.fire()
+    }
+    
+    func startTimerWithWeakSelf() {
+        customBlockTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: {[weak self] (timer) in
+            if let selfVC = self {
+                selfVC.makeCustomUnownedBlock()
+            }
+        })
+        customBlockTimer?.fire()
     }
 }
